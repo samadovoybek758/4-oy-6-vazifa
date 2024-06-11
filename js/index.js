5e646ce2c9f6e01310903a309498f4290c2d4e4e
samadovoybek758 <oybek22samadov@gmail.com>
2024-06-11 22:39:41 +0500

    firstly

---
+++ js/index.js
@@ -0,0 +1,216 @@
+// 1-masala
+// function yig(a,b) {
+//     return a+b
+// };
+// let a = +prompt("sonni kiritng");
+// let b = +prompt("sonni kiritng");
+// console.log(yig (a,b));
+
+
+// 2-masala
+// function kup(a,b) {
+//     return  a*b;
+// };
+// let a = +prompt("sonni kiritng");
+// let b = +prompt("sonni kiritng");
+// console.log(kup(a,b));
+
+
+// 3-masala
+// function kvad(a) {
+//     return a**2;
+// };
+// let a = +prompt("sonni kiritng");
+// console.log(kvad(a));
+
+
+
+// 4-masala
+// function kub(a) {
+//     return a**3;
+// };
+// let a = +prompt("sonni kiritng");
+// console.log(kub(a));
+
+
+
+// 5-masala
+// function urta(a,b,c) {
+//     return (a+b+c)/3
+// }
+// let a = +prompt("a sonni kiriting");;
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting")
+// console.log(urta(a,b,c));
+
+
+
+// 6-masala
+// function juft(a) {
+//     if (a%2==0) {
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+// };
+// let a = +prompt("sonni kiritng");
+// console.log(juft(a));
+
+
+
+// 7-masala
+// function toq(a){
+//     if (a%2==1) {
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+// };
+// let a = +prompt("sonni kiritng");
+// console.log(toq(a));
+
+
+
+
+// 8-masala
+// function bigger(a,b) {
+//     if (a>b) {
+//         return a
+//     }else{
+//         return b
+//     }
+// }
+// let a = +prompt("a sonni kiritng");
+// let b = +prompt("b sonni kiritng");
+// console.log(bigger(a,b));
+
+
+
+// 9-masala
+// function bigger(a,b) {
+//     if (a>b) {
+//         return b
+//     }else{
+//         return a
+//     }
+// }
+// let a = +prompt("a sonni kiritng");
+// let b = +prompt("b sonni kiritng");
+// console.log(bigger(a,b));
+
+
+
+
+// 10-masala
+// function yig(a) {
+//     let res = 0;
+//     for (let i = 1; i <=a; i++) {
+//         res = res+i
+//     }
+//     return res
+// }
+// let a = +prompt("sonni kiritng");
+// console.log(yig(a));
+
+
+
+// 11-masala
+// function fak(a) {
+//     let res = 1;
+//     for (let i = 1; i <=a; i++) {
+//         res = res*i
+//     }
+//     return res
+// }
+// let a = +prompt("sonni kiritng");
+// console.log(fak(a));
+
+
+
+// 12-masala
+// function yosh(a) {
+//     if (a>=18) {
+//         return "Katta"
+//     }else{
+//         return "Kichik"
+//     }
+// }
+// let a = +prompt("sonni kiritng");
+// console.log(yosh(a));
+
+
+
+// 13-masala
+// function bul(a,b) {
+//     if (a%b==0 || b%a==0) {
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+// }
+// let a = +prompt("a sonni kiritng");
+// let b = +prompt("b sonni kiritng")
+// console.log(bul(a,b));
+
+
+
+// 14-masala
+// function mus(a) {
+//     if (a>0) {
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+// }
+// let a = +prompt("sonni kiritng");
+// console.log(mus(a));
+
+
+
+// 15-masala
+// function manfiy(a) {
+//     if (a<0) {
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+// }
+// let a = +prompt("sonni kiritng");
+// console.log(manfiy(a));
+
+
+
+// 16-masala
+
+
+
+// 17-masala
+// function farq(a,b) {
+//     if (a-b>0) {
+//         return a-b
+//     }else{
+//         return b-a
+//     }
+// }
+// let a = +prompt("a sonni kiritng");
+// let b = +prompt("b sonni kiritng");
+// console.log(farq(a,b));
+
+
+
+
+// 18-masala
+function biggest(a,b,c) {
+    if (a>b && a>c) {
+        return a
+    }if (b>a && b>c) {
+        return b
+    }if (c>b && c>a) {
+        return c
+    }
+}
+let a = +prompt("a sonni kiritng");
+let b = +prompt("b sonni kiritng");
+let c = +prompt("c sonni kiritng");
+console.log(biggest(a,b,c));
+
